e1c18e3682eee79d391904e38ad31a5d67b7f8cf
Denisa Pantelimon <denisa.pantelimon@yahoo.com>
2023-02-08 17:19:38 +0200

    Initial commit

---
+++ coding-challenge/src/empty.ts
@@ -0,0 +1,2 @@
+// This file is not a mistake :)
+// It's purpose is to avoid the following error "No inputs were found in config file..."
---
+++ index.js
@@ -0,0 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const test = uuidv4();
+console.log(test);
